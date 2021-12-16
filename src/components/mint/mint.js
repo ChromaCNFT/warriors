36fd4451854f5f801a102b310d13f0feb145d6eb
AlexAltrex <alexaltrex@mail.ru>
2021-12-16 20:43:33 +0300

    add

--- src/components/mint/mint.js
+++ src/components/mint/mint.js
@@ -41,39 +41,41 @@ const Mint = () => {
     return (
         <div className='mint'>
 
-            <div className="mountain" style={{backgroundImage:`url(${mountain})`}} data-aos="fade-right">
+            <div className="mountain" style={{backgroundImage: `url(${mountain})`}} data-aos="fade-right">
                 <div className="des">
                     <h1>Build your Warrior!</h1>
                     <p>
-                        Travel back in time and join us on the journey 
-                        of fierce and loyal warriors. With Century Warriors 
-                        NFT, you have an opportunity to create your own 
+                        Travel back in time and join us on the journey
+                        of fierce and loyal warriors. With Century Warriors
+                        NFT, you have an opportunity to create your own
                         warrior with incredible hand drawn art
                     </p>
-                    <HashLink to='/ver'
-                              smooth={true}
+                    <HashLink
+                        to='/'
+                        //to='/ver'
+                        smooth={true}
                     >
                         MINT
                     </HashLink>
                 </div>
                 <div className="image">
-                    <img src={herous} alt="" />
+                    <img src={herous} alt=""/>
                 </div>
             </div>
-            <div className="series" data-aos="fade-up" style={{backgroundImage:`url(${series})`}}>
+            <div className="series" data-aos="fade-up" style={{backgroundImage: `url(${series})`}}>
                 <div className="title">Series 1: Drop 1:</div>
                 <div className="date" id='date'>
                     <span>{convertToTwoDigit(getDays(time))}</span>
-                    <span style={{ color: 'grey'}}> Days</span>
-                    <span style={{ color: 'white'}}> : </span>
+                    <span style={{color: 'grey'}}> Days</span>
+                    <span style={{color: 'white'}}> : </span>
                     <span>{convertToTwoDigit(getHours(time))}</span>
-                    <span style={{ color: 'grey'}}> HOURS</span>
-                    <span style={{ color: 'white'}}> : </span>
+                    <span style={{color: 'grey'}}> HOURS</span>
+                    <span style={{color: 'white'}}> : </span>
                     <span>{convertToTwoDigit(getMins(time))}</span>
-                    <span style={{ color: 'grey'}}> minutes</span>
-                    <span style={{ color: 'white'}}> : </span>
+                    <span style={{color: 'grey'}}> minutes</span>
+                    <span style={{color: 'white'}}> : </span>
                     <span>{convertToTwoDigit(getSecs(time))}</span>
-                    <span style={{ color: 'grey'}}> seconds</span>
+                    <span style={{color: 'grey'}}> seconds</span>
                 </div>
             </div>
             <div className="price">
